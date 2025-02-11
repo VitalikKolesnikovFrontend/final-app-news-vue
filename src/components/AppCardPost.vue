8e9b7839502fb0b6a83696852fb8284483a83140
Vitalik <Kolesnikov.frontend@gmail.com>
2025-02-11 17:41:36 +0300

    added rendering of posts from an array

---
+++ src/components/AppCardPost.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  content: String,
+  author: String,
+  createdAt: Date,
+  tags: String,
+})
+</script>
+<template>
+  <el-card style="max-width: 480px">
+    <template #header>{{ props.title }}</template>
+    <img
+      src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+      style="width: 100%"
+    />
+    <p>{{ props.content }}</p>
+    <p>{{ props.createdAt }}</p>
+    <p>{{ props.tags }}</p>
+    <template #footer>{{ props.author }}</template>
+  </el-card>
+</template>
+<style scoped></style>
---
+++ src/components/AppListPost.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from 'vue'
+import AppCardPost from './AppCardPost.vue'
+
+const posts = ref([
+  {
+    id: 2,
+    title: 'hello world',
+    content: 'hello world! i like js ',
+    createdAt: '2025-02-11T13:04:23.309Z',
+    tags: [],
+    author: {
+      id: 1,
+      login: 'vitalik',
+      avatarUrl: null,
+      createdAt: '2025-02-11T13:04:23.221Z',
+    },
+  },
+  {
+    id: 2,
+    title: 'hello world',
+    content: 'hello world! i like js ',
+    createdAt: '2025-02-11T13:04:23.309Z',
+    tags: [],
+    author: {
+      id: 1,
+      login: 'vitalik',
+      avatarUrl: null,
+      createdAt: '2025-02-11T13:04:23.221Z',
+    },
+  },
+  {
+    id: 2,
+    title: 'hello world',
+    content: 'hello world! i like js ',
+    createdAt: '2025-02-11T13:04:23.309Z',
+    tags: [],
+    author: {
+      id: 1,
+      login: 'vitalik',
+      avatarUrl: null,
+      createdAt: '2025-02-11T13:04:23.221Z',
+    },
+  },
+])
+</script>
+<template>
+  <div class="main">
+    <ul v-for="item in posts" :key="item.id">
+      <AppCardPost
+        :title="item.title"
+        :content="item.content"
+        :author="item.author.login"
+        :createdAt="item.createdAt"
+        :tags="item.tags[i]"
+      />
+    </ul>
+  </div>
+</template>
+<style scoped>
+.main {
+  display: flex;
+}
+</style>
